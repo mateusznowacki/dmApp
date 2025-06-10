57778132495c3739944547ff530234866859e134
mateusz-nowacki <mattynovy@outlook.com>
2025-06-10 23:20:03 +0200

    Initial commit

---
+++ src/main/java/com/dmApp/DmAppApplication.java
@@ -0,0 +1,13 @@
+package com.dmApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmApp/DmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
